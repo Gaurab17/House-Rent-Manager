36f8ad4ff42a5728c645006190f5dac9a72ebaf9
myusername <myemail>
2022-02-12 03:15:09 +0545

    Sth Changed

--- housemanager/lib/Screens/Home/home.dart
+++ housemanager/lib/Screens/Home/home.dart
@@ -37,7 +37,7 @@ class _HomeState extends State<Home> {
           TextButton.icon(
             onPressed: () {},
             icon: Icon(
-              Icons.notifications,
+              Icons.home,
               color: Colors.black,
             ),
             label: Text(''),
