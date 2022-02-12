a87d4285dc04560743416f9b0618bdc4a5d8eb0c
Gaurab17 <gaurabstha001@gmail.com>
2022-02-12 13:19:31 +0545

    Git push error trial

--- housemanager/lib/Screens/Home/home.dart
+++ housemanager/lib/Screens/Home/home.dart
@@ -37,7 +37,7 @@ class _HomeState extends State<Home> {
           TextButton.icon(
             onPressed: () {},
             icon: Icon(
-              Icons.home,
+              Icons.notifications,
               color: Colors.black,
             ),
             label: Text(''),
